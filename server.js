1d44bd915dd7b64ae9f16893980313ce23c6b04a
vardhanh577 <vardhanh577@yahoo.in>
2017-08-14 23:36:27 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+app.get('/ss', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'ss.html'));
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
