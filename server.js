f8080ab14d57612bb10d68814895936ab0831842
vardhanh577 <vardhanh577@yahoo.in>
2017-08-15 19:23:36 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -86,7 +86,7 @@ app.get('/hash/:input', function(req, res) {
 
 app.post('/create-user', function (req, res) {
    // username, password
-   // {"username": "tanmai", "password": "password"}
+   // {"username": "harsha", "password": "password"}
    // JSON
    var username = req.body.username;
    var password = req.body.password;
